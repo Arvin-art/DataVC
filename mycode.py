5196eeb653ecf626a53e0e811293ec5a0ff6e536
Arvind Gongati <121613122+ar-vin99@users.noreply.github.com>
2025-04-14 16:21:00 -0400

    First commit before DVC

---
+++ mycode.py
@@ -0,0 +1,20 @@
+import pandas as pd
+import numpy as np
+import os
+
+data = {'Name': ['Alice', 'Bob', 'Charlie', 'David'],
+        'Age': [24, 30, 22, 35],
+        'City': ['New York', 'Los Angeles', 'Chicago', 'Houston']}
+
+df = pd.DataFrame(data)
+print("Original DataFrame:")
+print(df)
+
+data_dir = 'data'
+if not os.path.exists(data_dir):
+    os.makedirs(data_dir)
+
+file_path = os.path.join(data_dir, 'data.csv')
+
+df.to_csv(file_path, index=False)
+print(f"\nDataFrame saved to {file_path}")
